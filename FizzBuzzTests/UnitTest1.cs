ed86e9e67440bc4a3d3dc5dbee9a241d755a08a9
Apric0t82 <apric0t@outlook.com>
2025-02-25 00:07:25 +0300

    add test

--- FizzBuzzTests/UnitTest1.cs
+++ FizzBuzzTests/UnitTest1.cs
@@ -27,6 +27,7 @@ namespace Algorithms
         [Theory]
         [InlineData(5, "1\n2\nfizz\n4\nbuzz\n")]
         [InlineData(15, "1\n2\nfizz\n4\nbuzz\nfizz\n7\n8\nfizz\nbuzz\n11\nfizz\n13\n14\nfizzbuzz\n")]
+        [InlineData(20, "1\n2\nfizz\n4\nbuzz\nfizz\n7\n8\nfizz\nbuzz\n11\nfizz\n13\n14\nfizzbuzz\n16\n17\nfizz\n19\nbuzz\n")]
         public void RunFizzBuzzFunctionalTests(int input, string expected)
         {
             // Arrange
